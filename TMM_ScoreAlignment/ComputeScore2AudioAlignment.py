8e26d7e42f4a35dea4568f3a5f5bed18aa665b42
Sankalp Gulati <sankalp.gulati@upf.edu>
2014-02-17 21:28:07 +0100

    Added path plotting function for DTW

--- TMM_ScoreAlignment/ComputeScore2AudioAlignment.py
+++ TMM_ScoreAlignment/ComputeScore2AudioAlignment.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys,os
 import copy
 import time
+import matplotlib.pyplot as plt
 
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../library_pythonnew/batchProcessing/'))
@@ -10,6 +11,36 @@ from scipy.interpolate import interp1d
 import batchProcessing as BP
 import dtw
 
+
+def drawAlignment(x , y , path):
+    
+    plt.plot(x+53, color='b')
+    plt.hold(True)
+    plt.plot(y,color='g')
+    
+    plt.figure()
+    plt.plot(x+53, color='b')
+    plt.hold(True)
+    plt.plot(y,color='g')
+    for ii, row in enumerate(path[0]):
+        
+        if ii%10==0:
+            x1 = path[0][ii]
+            y1 = x[path[0][ii]]+53
+            
+            x2 = path[1][ii]
+            y2 = y[path[1][ii]]
+            
+            plt.plot([x1,x2], [y1,y2], color='r')
+        
+        
+    plt.show()
+        
+        
+        
+        
+    
+    
 
 def ComputeScore2AudioAlignment(queryFile, targetFile):
     
@@ -28,6 +59,7 @@ def ComputeScore2AudioAlignment(queryFile, targetFile):
     time = np.transpose(np.array([intFunc(factorArray)]))
     queryData = np.concatenate((time, pitch),axis=1)
     
+    
     #removing silence regions or anything undesirable from both the 
     indSil = np.isfinite(queryData[:,1])
     indSil = np.where(indSil==False)[0]
@@ -37,7 +69,10 @@ def ComputeScore2AudioAlignment(queryFile, targetFile):
     indSil = np.where(indSil==False)[0]
     targetDataDel = np.delete(targetData, [indSil],axis=0)
     
-    dist, pathLen, path, cost = dtw.dtw1d(queryDataDel[:,1], targetDataDel[:,1], {'Output':4, 'Ldistance':{'type':2}, 'Constraint':{'type':''}})
+    dist, pathLen, path, cost = dtw.dtw1dSubLocalBand(queryDataDel[:,1], targetDataDel[:,1], {'Output':4, 'Ldistance':{'type':2}, 'Constraint':{'CVal':int(queryDataDel.shape[0]*0.2)}})
+    
+    
+    drawAlignment(queryDataDel[:,1], targetDataDel[:,1], path)
     
     timeStampsQuery = queryDataDel[:,0][path[0]]
     timeStampsTarget = targetDataDel[:,0][path[1]]
