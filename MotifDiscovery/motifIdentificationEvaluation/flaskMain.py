e5aa5ed1763ee9b3e5a4731d78bef9149e6ee4fd
sankalpg <sankalp.gulati@gmail.com>
2014-03-29 13:15:11 +0100

    Adding stuff for Eval web

--- MotifDiscovery/motifIdentificationEvaluation/flaskMain.py
+++ MotifDiscovery/motifIdentificationEvaluation/flaskMain.py
@@ -1,6 +1,6 @@
 import flask, flask.views
 import os, functools
-import fetchDataPSQL as psqlGet
+#import fetchDataPSQL as psqlGet
 from flask import request
 import numpy as np
 
@@ -13,7 +13,19 @@ nPatternPerSeed = 10
 
 class  Main(flask.views.MethodView):
     def get(self):
-        return flask.render_template('index.html')
+        #read the evaluation data
+        EvalInfo = np.loadtxt('evaluation.txt')
+        indIncomplete = np.where(EvalInfo[1:,:]==-1)[0]
+        
+        totalSearchPatterns = EvalInfo[1:,:].size
+        searchPatternsDone = totalSearchPatterns - indIncomplete.size
+
+        indIncomplete = np.where(EvalInfo[0,:]==-1)[0]
+        totalSeedPatterns = EvalInfo[0,:].size
+        seedPatternsDone = totalSeedPatterns - indIncomplete.size
+        
+
+        return flask.render_template('index.html', seedPatternsDone = seedPatternsDone, totalSeedPatterns = totalSeedPatterns, searchPatternsDone=searchPatternsDone, totalSearchPatterns=totalSearchPatterns)
 
 class  Seed(flask.views.MethodView):
     def get(self):
@@ -23,15 +35,48 @@ class  Seed(flask.views.MethodView):
 class  Version(flask.views.MethodView):
     def get(self):
         return flask.render_template('version.html')
+
+class  Search(flask.views.MethodView):
+    def get(self):
+        return flask.render_template('search.html')
     
     
+@app.route('/searchPage', methods=['GET', 'POST'])
+def searchPage():
+    
+    seedInd = int(request.args.get('seedIndex'))-1
+
+    versionInd = int(request.args.get('version'))-1
+    
+    if request.method=='POST':
+        searchInd = int(request.args.get('searchIndex'))-1
+        rating = int(request.form['rating'])
+        EvalInfo = np.loadtxt('evaluation.txt')
+        EvalInfo[1+(nPatternPerSeed*versionInd) + searchInd,seedInd]=rating
+        np.savetxt('evaluation.txt',EvalInfo)
+        
+    #read the evaluation data
+    patternInfo = np.loadtxt('pattern.txt')
+    EvalInfo = np.loadtxt('evaluation.txt')
+    
+    doneArray = np.ones(nPatternPerSeed).astype(np.int)
+
+    EvalInfo = EvalInfo[1+(versionInd*nPatternPerSeed):1+((versionInd+1)*nPatternPerSeed),seedInd]
+
+    indIncomplete = np.where(EvalInfo==-1)[0]
+
+    doneArray[indIncomplete] = 0
+    
+    return flask.render_template('search.html', searchPatterns = (np.arange(nPatternPerSeed)+1).tolist(), progress = doneArray, seedIndex=seedInd+1, version = versionInd+1)
+    
+
 @app.route('/versionPage', methods=['GET', 'POST'])
 def versionPage():
     
     seedInd = int(request.args.get('seedIndex'))-1
     
     if request.method=='POST':
-        rating = request.form['rating']
+        rating = int(request.form['rating'])
         EvalInfo = np.loadtxt('evaluation.txt')
         EvalInfo[0,seedInd]=rating
         np.savetxt('evaluation.txt',EvalInfo)
@@ -84,6 +129,9 @@ app.add_url_rule('/',
                 methods = ['GET'])
 app.add_url_rule('/', 
                 view_func=Version.as_view('version'), 
+                methods = ['GET', 'POST'])
+app.add_url_rule('/', 
+                view_func=Search.as_view('search'), 
                 methods = ['GET', 'POST'])
 
 
