3142cc7ebe3196b10851f726404ca82e789996d2
sankalpg <sankalp.gulati@gmail.com>
2014-04-16 02:17:11 +0200

    Added plot for ACR histogram ISMIR 2014

--- MotifDiscovery/ISMIR2014/parameterSelection/SamplingRatePitch/samplingRateExp.py
+++ MotifDiscovery/ISMIR2014/parameterSelection/SamplingRatePitch/samplingRateExp.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../library_pythonnew/batchProcessing/'))
 
-import batchProcessing as BP
+#import batchProcessing as BP
 
 #Python wrapper around ACR
 def obtainAutoCorr(pitch, nPoints):
@@ -124,26 +124,64 @@ def batchRunACRExtraction(root_dir, exePath):
         os.system(cmd)
         
 
-def plotAgg2dACRHistogramWithContours(histFile):
+def plotAgg2dACRHistogramWithContours(histFile, plotName=-1):
     """
     This function plots 2 d histogram of ACR values along with contours which represent the % of total popoulation below that ACR value. A contour of 80% will represent a line where 80% of the patterns ACR value is below this line.
 
     """
-    cumulativeValues = [25 50 75]
+    cumulativeValues = [75, 50, 25]
+    CategoryNames = ['3Q', '2Q', '1Q']
 
     data = np.load(histFile)
+    nTotal = np.sum(data[:,0])
     contours = np.zeros((len(cumulativeValues),data.shape[1]))
     #lets get ACR VALUE at every lag where the cumulative distribution becomes more than cumulativeValues % of the total number of excerpts
     for ii in range(data.shape[1]):
-        cumulateDist = [np.sum(data[0:k,ii] for k in range(data.shape[0])]
+        cumulateDist = [np.sum(data[k:data.shape[0],ii]) for k in range(data.shape[0])]
         cumValCrossInd = []
-        for val in cumulativeValues:
-            contours[jj,ii] = min(np.where(cumulateDist>=val)[0])
+        for jj, val in enumerate(cumulativeValues):
+            
+            ind= np.where(cumulateDist<=val*nTotal/100.0)[0]
+            if len(ind)==0:
+                contours[jj,ii] = data.shape[0]-1
+            else:
+                contours[jj,ii] = np.min(ind)
 
-
-    plt.imshow(np.log(hist2D+1), cmap=plt.cm.hot, aspect='auto')
+    fig = plt.figure() 
+    ax = fig.add_subplot(111)
     
-    plt,show()
+    plt.imshow(np.power(data, 1/8.0), cmap=plt.cm.hot, aspect='auto')
+    pLeg = []
+    for ii in range(contours.shape[0]):
+        p, = plt.plot(range(30), contours[ii,:], linewidth=3)
+        pLeg.append(p)
+
+    fsize = 20
+    fsize2 = 14
+    font="Times New Roman"
+    
+    plt.xlabel("Lag (samples)", fontsize = fsize, fontname=font)
+    plt.ylabel("ACR Histogram", fontsize = fsize, fontname=font, labelpad=fsize2)
+
+    plt.yticks(np.append(np.arange(0, 100, 10),99), ["%1.1f"%(p/100.0) for p in range(0,110,10)])
+
+    plt.legend(pLeg, [CategoryNames[pp] for pp in range(len(CategoryNames))], loc ='upper right', ncol = 3, fontsize = fsize2, scatterpoints=1, frameon=True, borderaxespad=0.1)
+    
+    ax.set_xlim([0,29])
+    ax.set_ylim([0,99])
+
+    xLim = ax.get_xlim()
+    yLim = ax.get_ylim()
+    
+    ax.set_aspect((xLim[1]-xLim[0])/(2*float(yLim[1]-yLim[0])))
+    plt.tick_params(axis='both', which='major', labelsize=fsize2)
+    
+    if isinstance(plotName, int):
+        plt.show()
+    elif isinstance(plotName, str):
+        fig.savefig(plotName)
+
+    return 1
 
 
 
