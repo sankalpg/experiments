ebcfa1f472b55b537317a79721560e71d57584c7
sankalp <sankalp.gulati@gmail.com>
2014-07-29 07:38:55 +0200

    Add intra doublet similarity code

--- PekingOpera/DoubletAnalysis/doubletAnalysis.py
+++ PekingOpera/DoubletAnalysis/doubletAnalysis.py
@@ -1,6 +1,9 @@
 import numpy as np
-
+import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../library_pythonnew/similarityMeasures/dtw/'))
+import matplotlib.pyplot as plt
+import dtw
+
 
 def find_nearest_element_ind(array,value):
         idx = (np.abs(array-value)).argmin()
@@ -55,9 +58,45 @@ def computeIntraDoubletSimilarities(baseName, annotExt, pitchExt):
                 s2 = find_nearest_element_ind(timePitch[:,0], doublet[parts[nn]][0])
                 e2 = find_nearest_element_ind(timePitch[:,0], doublet[parts[nn]][1])
                 
-                dist = 
+                q = timePitch[s1:e1,1]
+                t = timePitch[s2:e2,1]
+                
+                q = np.delete(q,np.where(q<0))
+                t = np.delete(t,np.where(t<0))
+                
+                q = 1200*np.log2(q/55)
+                t = 1200*np.log2(t/55)
+                
+                dist, pathLen, path, cost = dtw.dtw1dSubLocalBand(q,t, {'Output':4, 'Ldistance':{'type':1}, 'Constraint':{'CVal':int((e1-s1)*0.3)}})
+                print (parts[mm],parts[nn]), dist/pathLen
+                drawAlignment(q,t,path )
+                
+                
+                
             
+def drawAlignment(x , y , path):
+    
+    plt.plot(x, color='b')
+    plt.hold(True)
+    plt.plot(y,color='g')
+    
+    plt.figure()
+    plt.plot(x, color='b')
+    plt.hold(True)
+    plt.plot(y,color='g')
+    for ii, row in enumerate(path[0]):
         
+        if ii%10==0:
+            x1 = path[0][ii]
+            y1 = x[path[0][ii]]
+            
+            x2 = path[1][ii]
+            y2 = y[path[1][ii]]
+            
+            plt.plot([x1,x2], [y1,y2], color='r')
+        
+        
+    plt.show()        
         
     
     
