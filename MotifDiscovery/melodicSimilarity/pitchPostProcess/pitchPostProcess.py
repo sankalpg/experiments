f772bf305f3f75df1dc1e454c72670d9b1d464f5
sankalp <sankalp.gulati@gmail.com>
2014-12-16 13:44:54 +0100

    Add file for post processing pitch basic funcs

---
+++ MotifDiscovery/melodicSimilarity/pitchPostProcess/pitchPostProcess.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+
+
+###########################1) Median filtering pitch contour ###########################
+#Motivation
+#1) Remove spurious jumps in pitch due to octave errors or other errors
+#2) Supress the extent of ornaments like Kan swar which arise challenges to segmentation process or in identification of stable note regions
+#3) In general to smooth out pitch content supressing fine movements which might not be relevant for melodic similarity.
+
+def 
