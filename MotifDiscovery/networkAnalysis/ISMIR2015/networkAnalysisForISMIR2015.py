07af427353405ced75eaaf7a4caed6ba53c48c6a
Sankalp Gulati <sankalp.gulati@upf.edu>
2015-04-09 21:05:51 +0200

    Add function for batch process filtering

--- MotifDiscovery/networkAnalysis/ISMIR2015/networkAnalysisForISMIR2015.py
+++ MotifDiscovery/networkAnalysis/ISMIR2015/networkAnalysisForISMIR2015.py
@@ -4,6 +4,7 @@ import numpy as np
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../library_pythonnew/networkAnalysis/'))
 
 import constructNetwork as cons
+import networkProcessing as netPro
 
 def batchGenerateAllNetworkVariants(out_dir, fileListFile, pattDistExt, wghts = [-1, 0, 1], d_thslds = [30]):
     """
@@ -25,3 +26,20 @@ def batchGenerateAllNetworkVariants(out_dir, fileListFile, pattDistExt, wghts = 
             cons.constructNetwork_Weighted_NetworkX(fileListFile, os.path.join(out_dir, outfile), t, pattDistExt, w)
     
 
+def batchGenerateDisparityFilteredOutput(networkFile, outputDir, confs = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]):
+    """
+    This function (batch) generates disparity filtered networks.
+    The resultant network is stored in the output folder in a folder named confXX where XX is the confidence measure of the disparity filtering.
+    """
+    
+    for conf in confs:
+        conf_val = ((1-conf)*100.0)
+        outputFolder = os.path.join(outputDir, "conf%f"%conf_val)
+        
+        if not os.path.isdir(outputFolder):
+            os.makedirs(outputFolder)
+        fname, ext = os.path.splitext(networkFile)
+        outfile = os.path.join(outputFolder, fname+'___Conf_%f'%conf_val+ext)
+        netPro.filterAndSaveNetwork(networkFile, outfile, conf)
+    
+    
