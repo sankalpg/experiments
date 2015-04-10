e97f7d9bf5ef13c94ae16aee0aa3510c6b6a7c3f
Sankalp Gulati <sankalp.gulati@upf.edu>
2015-04-10 12:04:07 +0200

    Changed network variant generation code

--- MotifDiscovery/networkAnalysis/ISMIR2015/networkAnalysisForISMIR2015.py
+++ MotifDiscovery/networkAnalysis/ISMIR2015/networkAnalysisForISMIR2015.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../library_pyt
 import constructNetwork as cons
 import networkProcessing as netPro
 
-def batchGenerateAllNetworkVariants(out_dir, fileListFile, pattDistExt, wghts = [-1, 0, 1], d_thslds = [30]):
+def batchGenerateAllNetworkVariants(out_dir, fileListFile, pattDistExt, wghts = [0], d_thslds = [30], confs =[-1, 0.01, 0.1, 0.2, 0.5, 0.7, 0.9]):
     """
     This function generates different network variants from the patterns that we have obtained from
     the unsupervised analysis
@@ -19,27 +19,26 @@ def batchGenerateAllNetworkVariants(out_dir, fileListFile, pattDistExt, wghts = 
 
     outputfile naming convention Weight_XX_DThsld_YY_NX.edges
     """
-
-    for w in wghts:
-        for t in d_thslds:
-            outfile = "Weight_%d___DThsld_%d___NX.edges"%(w, t)
-            cons.constructNetwork_Weighted_NetworkX(fileListFile, os.path.join(out_dir, outfile), t, pattDistExt, w)
-    
-
-def batchGenerateDisparityFilteredOutput(networkFile, outputDir, confs = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]):
-    """
-    This function (batch) generates disparity filtered networks.
-    The resultant network is stored in the output folder in a folder named confXX where XX is the confidence measure of the disparity filtering.
-    """
-    
     for conf in confs:
         conf_val = ((1-conf)*100.0)
-        outputFolder = os.path.join(outputDir, "conf%f"%conf_val)
+        outputFolder = os.path.join(out_dir, "conf%0.2f"%conf_val)
         
         if not os.path.isdir(outputFolder):
             os.makedirs(outputFolder)
-        fname, ext = os.path.splitext(networkFile)
-        outfile = os.path.join(outputFolder, fname+'___Conf_%f'%conf_val+ext)
+        
+        for w in wghts:
+            for t in d_thslds:
+                networkFile = os.path.join(outputFolder, "Weight_%d___DThsld_%d___Conf_%0.2f___NX.edges"%(w, t, conf_val))
+                cons.constructNetwork_Weighted_NetworkX(fileListFile, networkFile, t, pattDistExt, w, conf)
+    
+    
+    
+"""
+def batchGenerateDisparityFilteredOutput(networkFile, outputDir, confs = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]):
+        This function (batch) generates disparity filtered networks.
+    The resultant network is stored in the output folder in a folder named confXX where XX is the confidence measure of the disparity filtering.
+    
+    
+    
         netPro.filterAndSaveNetwork(networkFile, outfile, conf)
-    
-    
+""" 
