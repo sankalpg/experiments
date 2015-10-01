3e3a7c8733c4cc8ed7694d78faf07aca4d6213a8
Sankalp Gulati <sankalp.gulati@upf.edu>
2015-10-01 12:16:01 +0200

    Introduced small info box

--- MotifDiscovery/ICASSP2016/raga_recognition/network_visualization/api/api.py
+++ MotifDiscovery/ICASSP2016/raga_recognition/network_visualization/api/api.py
@@ -1,11 +1,15 @@
 from __future__ import unicode_literals
 from flask import Flask, request, jsonify, current_app
+import requests
 import psycopg2 as psy
 from functools import wraps
 import sys
 import os.path
 import json
 from flask.ext.cors import CORS
+from compmusic import dunya as dn
+
+auth_token = "60312f59428916bb854adaa208f55eb35c3f2f07"
 
 app = Flask(__name__)
 CORS(app)
@@ -41,6 +45,15 @@ def get_phrase_data():
     out = {'mbid':mbid, 'ragaid': raaga, 'start':start, 'end':end}
     
     return jsonify(**out)
+
+@app.route('/get_rec_data', methods=['GET', 'POST'])
+@support_jsonp
+def get_rec_data():
+    mbid = request.args.get('mbid')
+    url = "http://dunya.compmusic.upf.edu/api/carnatic/recording/" + mbid
+    data = requests.get(url, headers = {"Authorization":"Token " + auth_token})
+    print data.json()
+    return jsonify(**(data.json()))
 
 
 if __name__ == '__main__':
