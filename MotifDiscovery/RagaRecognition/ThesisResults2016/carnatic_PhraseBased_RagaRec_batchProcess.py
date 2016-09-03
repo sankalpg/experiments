47ec954d1cee0735cd4272918c1e589a3a7117c1
Sankalp Gulati <sankalp.gulati@upf.edu>
2016-09-03 23:17:38 +0200

    Added plotting file, revised version of the prev one

--- MotifDiscovery/RagaRecognition/ThesisResults2016/carnatic_PhraseBased_RagaRec_batchProcess.py
+++ MotifDiscovery/RagaRecognition/ThesisResults2016/carnatic_PhraseBased_RagaRec_batchProcess.py
@@ -26,14 +26,15 @@ def run_raga_recognition_V2_THESIS_RESULT_PhraseSection_GRID_CARNATIC():
     pre_processing = [2]
     norm_tfidf = [None]
     smooth_idf = [False]
-    classifier = [('svm', "default"),
-                  ('svm-lin', "default"),
-                  ('sgd', "default"),
-                  ('nb-multi', "default"),
-                  ('nb-gauss', "default"),
-                  ('nb-bern', "default"),
-                  ('randForest', "default"),
-                  ('logReg', "default")]
+    classifier = [('nb-multi', "default")]
+#    classifier = [('svm', "default"),
+#                  ('svm-lin', "default"),
+#                  ('sgd', "default"),
+#                  ('nb-multi', "default"),
+#                  ('nb-gauss', "default"),
+#                  ('nb-bern', "default"),
+#                  ('randForest', "default"),
+#                  ('logReg', "default")]
     n_expts = 1
     database = {'.pattDistance_2s_config0':'Raga_Rec_Carnatic_40Raga_Config0',
                 '.pattDistance_2s_config1':'Raga_Rec_Carnatic_40Raga_Config1',
@@ -84,4 +85,143 @@ def run_raga_recognition_V2_THESIS_RESULT_PhraseSection_GRID_CARNATIC():
 
 
 
+
+def run_raga_recognition_V2_THESIS_RESULT_PhraseSection_PAPERTABLE_CARNATIC():
+    
+	#Best in ISMIR2016:    8	.pattDistance_2s_config3	tf-idf	2	None	False	1	0.68125
+
+    #parameters
+    out_dir = '/home/sankalp/Work/Work_PhD/experiments/MotifDiscovery/RagaRecognition/ThesisResults2016/PhraseBased/Carnatic/paperTable'
+    fileListFile = '/media/Data/Datasets/PatternProcessing_DB/unsupervisedDBs/carnaticDB/Carnatic40RagaICASSP2016/__dbInfo__/Carnatic40RagasICASSP2016.flist_local'
+    scratch_dir = '/home/sankalp/Work/Work_PhD/experiments/MotifDiscovery/RagaRecognition/ThesisResults2016/PhraseBased/Carnatic/scratch'
+    thresholdBin = [8]
+    pattDistExt = ['.pattDistance_2s_config3']
+    network_wght_type = -1
+    force_build_network=0
+    feature_type = ['tf', 'tp', 'tf-idf']
+    pre_processing = [2]
+    norm_tfidf = [None]
+    smooth_idf = [False]
+    classifier = [('svm', "default"),
+                    ('svm-lin', "default"),
+                    ('sgd', "default"),
+                    ('nb-multi', "default"),
+                    ('nb-gauss', "default"),
+                    ('nb-bern', "default"),
+                    ('randForest', "default"),
+                    ('logReg', "default")]
+    n_expts = 1
+    database = {'.pattDistance_2s_config0':'Raga_Rec_Carnatic_40Raga_Config0',
+                '.pattDistance_2s_config1':'Raga_Rec_Carnatic_40Raga_Config1',
+                '.pattDistance_2s_config2':'Raga_Rec_Carnatic_40Raga_Config2',
+                '.pattDistance_2s_config3':'Raga_Rec_Carnatic_40Raga_Config3'}
+
+    
+    fid = open(os.path.join(out_dir, 'results_summary.txt'),'w')
+    fid.write("%s\t"*8%("Threshold", "pattExt", "Feature", "pre-proc", "norm_tfidf", "smooth", "classifier", "accuracy"))
+    fid.write('\n')
+    cnt = 1
+    for t in thresholdBin:
+        for ext in pattDistExt:
+            for f in feature_type:
+                for p in pre_processing:
+                    for n in norm_tfidf:
+                        for s in smooth_idf:
+                            for ii, c in enumerate(classifier):
+                                dir_name = os.path.join(out_dir, "config_%s"%str(cnt))
+                                ###TEMP: remove this step, it was done because the process was stopped in the middle
+                                if os.path.isdir(dir_name):
+                                    results_data = pickle.load(open(os.path.join(dir_name, 'experiment_results.pkl'),'r'))
+                                    result = results_data['var2']['accuracy']
+                                else:
+                                    result = RR.raga_recognition_V2(dir_name, 
+                                                                    scratch_dir,
+                                                                    fileListFile,
+                                                                    t,
+                                                                    ext,
+                                                                    network_wght_type = network_wght_type,
+                                                                    force_build_network=force_build_network,
+                                                                    feature_type = f,
+                                                                    pre_processing = p,
+                                                                    norm_tfidf = n,
+                                                                    smooth_idf = s,
+                                                                    classifier = c,
+                                                                    n_expts = n_expts,
+                                                                    var1 = False,
+                                                                    var2 = True,
+                                                                    myDatabase = database[ext],
+                                                                    myUser = 'sankalp',
+                                                                    type_eval = ("LeaveOneOut", -1),
+                                                                    balance_classes = 1)
+                                cnt+=1
+                                fid.write("%s\t"*8%(str(t), ext, f, str(p), str(n), str(s), str(ii), str(result)))
+                                fid.write('\n')
+    fid.close()
+
+
+
+
+def run_raga_recognition_V2_THESIS_RESULT_PhraseSection_THRESHOLDS_CARNATIC():
+    
+	#Best in ISMIR2016:    8	.pattDistance_2s_config3	tf-idf	2	None	False	1	0.68125
+
+    #parameters
+    out_dir = '/home/sankalp/Work/Work_PhD/experiments/MotifDiscovery/RagaRecognition/ThesisResults2016/PhraseBased/Carnatic/thresholds'
+    fileListFile = '/media/Data/Datasets/PatternProcessing_DB/unsupervisedDBs/carnaticDB/Carnatic40RagaICASSP2016/__dbInfo__/Carnatic40RagasICASSP2016.flist_local'
+    scratch_dir = '/home/sankalp/Work/Work_PhD/experiments/MotifDiscovery/RagaRecognition/ThesisResults2016/PhraseBased/Carnatic/scratch'
+    thresholdBin = range(3,20)
+    pattDistExt = ['.pattDistance_2s_config3']
+    network_wght_type = -1
+    force_build_network=0
+    feature_type = ['tf-idf']
+    pre_processing = [2]
+    norm_tfidf = [None]
+    smooth_idf = [False]
+    classifier = [('nb-multi', "default")]
+    n_expts = 1
+    database = {'.pattDistance_2s_config0':'Raga_Rec_Carnatic_40Raga_Config0',
+                '.pattDistance_2s_config1':'Raga_Rec_Carnatic_40Raga_Config1',
+                '.pattDistance_2s_config2':'Raga_Rec_Carnatic_40Raga_Config2',
+                '.pattDistance_2s_config3':'Raga_Rec_Carnatic_40Raga_Config3'}
+
+    
+    fid = open(os.path.join(out_dir, 'results_summary.txt'),'w')
+    fid.write("%s\t"*8%("Threshold", "pattExt", "Feature", "pre-proc", "norm_tfidf", "smooth", "classifier", "accuracy"))
+    fid.write('\n')
+    cnt = 1
+    for t in thresholdBin:
+        for ext in pattDistExt:
+            for f in feature_type:
+                for p in pre_processing:
+                    for n in norm_tfidf:
+                        for s in smooth_idf:
+                            for ii, c in enumerate(classifier):
+                                dir_name = os.path.join(out_dir, "config_%s"%str(cnt))
+                                if os.path.isdir(dir_name):
+                                    results_data = pickle.load(open(os.path.join(dir_name, 'experiment_results.pkl'),'r'))
+                                    result = results_data['var2']['accuracy']
+                                else:
+                                    result = RR.raga_recognition_V2(dir_name, 
+                                                                    scratch_dir,
+                                                                    fileListFile,
+                                                                    t,
+                                                                    ext,
+                                                                    network_wght_type = network_wght_type,
+                                                                    force_build_network=force_build_network,
+                                                                    feature_type = f,
+                                                                    pre_processing = p,
+                                                                    norm_tfidf = n,
+                                                                    smooth_idf = s,
+                                                                    classifier = c,
+                                                                    n_expts = n_expts,
+                                                                    var1 = False,
+                                                                    var2 = True,
+                                                                    myDatabase = database[ext],
+                                                                    myUser = 'sankalp',
+                                                                    type_eval = ("LeaveOneOut", -1),
+                                                                    balance_classes = 1)
+                                cnt+=1
+                                fid.write("%s\t"*8%(str(t), ext, f, str(p), str(n), str(s), str(ii), str(result)))
+                                fid.write('\n')
+    fid.close()
 
---
+++ MotifDiscovery/RagaRecognition/ThesisResults2016/plotting_wrapper.py
@@ -0,0 +1,391 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import numpy as np
+import os,sys
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import json, pickle
+import codecs
+import matplotlib as mpl
+mpl.rc('font',family='Times New Roman')
+
+baseClusterCoffFileName = '_ClusteringCoff'
+randomizationSuffix = '_RANDOM'
+baseNetworkPropFileName = '_NetworkInfo'
+    
+def readClusterinCoffCurve(root_dir, nFiles,suffix=''):
+    """
+    This function just reads multiple files and accumulate CC values and returns it
+    """
+    CC = []
+    CC_Rand = []    
+    for ii in range(1, nFiles+1):
+
+        try:
+            cc = np.loadtxt(os.path.join(root_dir, str(ii)+baseClusterCoffFileName+suffix+'.txt'))
+            cc_rand = np.loadtxt(os.path.join(root_dir,str(ii)+baseClusterCoffFileName+randomizationSuffix+suffix+'.txt'))
+        except:
+            cc = 0
+            cc_rand = 0
+         
+        CC.append(cc)
+        CC_Rand.append(cc_rand)
+    
+    return CC, CC_Rand
+
+def plotClusteringCoff(root_dir, nFiles, plotName=-1, legData = []):
+    """
+    This function plots clustering cofficient as a function of threshold using which the network was build. It also plots the CC corresponding to the randomized network.
+    
+    """
+    cc, cc_rand = readClusterinCoffCurve(root_dir, nFiles)
+    
+    cc = np.array(cc[1:])   #because of 0 bin is 1 file
+    cc_rand = np.array(cc_rand[1:])
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.hold(True)
+    fsize = 18
+    fsize2 = 16
+    font="Times New Roman"
+    
+    plt.xlabel("$T_s$ (bin index)", fontsize = fsize, fontname=font)
+    plt.ylabel("Clustering Coefficient $C$", fontsize = fsize, fontname=font, labelpad=fsize2)
+    
+    
+    pLeg = []
+    
+    markers = ['.', 'o', 's', '^', '<', '>', 'p']    
+    colors = ['r', 'b', 'm', 'c', 'g', 'k']
+    colors_dotted = ['r--', 'b--', 'm--', 'c--', 'g--', 'k--']
+
+    p, = plt.plot(cc, 'b', linewidth=2)
+    pLeg.append(p)
+    p, = plt.plot(cc_rand, 'b--', linewidth=2)
+    pLeg.append(p)
+    p, = plt.plot(cc-cc_rand, 'r', linewidth=0.5, marker = '.')
+    pLeg.append(p)
+
+    ax.set_ylim([0,0.45])
+    ax.set_xlim([0,35])
+    
+    xLim = ax.get_xlim()
+    yLim = ax.get_ylim()
+    
+    ax.set_aspect((xLim[1]-xLim[0])/(2*float(yLim[1]-yLim[0])))
+    plt.legend(pLeg, ['$C(\mathcal{G})$','$C(\mathcal{G}_r)$', '$C(\mathcal{G})-C(\mathcal{G}_r)$'], fontsize = 15, loc=4, bbox_to_anchor=(1,0.1))
+    plt.tick_params(axis='both', which='major', labelsize=fsize2)
+    
+    if isinstance(plotName, int):
+        plt.show()
+    elif isinstance(plotName, str):
+        fig.savefig(plotName, bbox_inches='tight')
+        
+        
+#def plot_confusion_matrix(result_file, raga_uuid_name_file):
+
+
+def plot_confusion_matrix(raga_name_map_file, result_file, outputname):
+    
+    raga_name_map = json.load(codecs.open(raga_name_map_file,'r', encoding = 'utf-8'))
+    results = pickle.load(open(result_file, 'r'))
+    conf_arr = results['var2']['cm'][0]
+    raga_names = results['var2']['mapping']
+    
+    y_labels = []
+    x_labels = []
+    for ii, r in enumerate(raga_names):
+        y_labels.append('R'+str(ii+1))
+        x_labels.append(y_labels[-1] + '-'+raga_name_map[r])
+    
+    width = len(conf_arr)
+    height = len(conf_arr[0])
+ 
+    fig = plt.figure(figsize=(14,14))
+    #fig = plt.figure()
+    plt.clf()
+    ax = fig.add_subplot(111)
+    ax.set_aspect(1)
+    ax.grid(which='major')
+    cmap_local = plt.get_cmap('binary', np.max(conf_arr)-np.min(conf_arr)+1)
+    #res = ax.matshow(np.array(conf_arr), #cmap=plt.cm.binary, 
+    #                interpolation='nearest', aspect='1', cmap=cmap_local,
+    #                ##Commenting out this line sets labels correctly,
+    #                ##but the grid is off
+    #                extent=[0, width, height, 0],
+    #                vmin =np.min(conf_arr)-.5, vmax = np.max(conf_arr)+0.5,
+    #                )
+    res = ax.pcolor(np.array(conf_arr), cmap=cmap_local, edgecolor='black', linestyle=':', lw=1)
+    
+    ticks = np.arange(np.min(conf_arr),np.max(conf_arr)+2)
+    tickpos = np.linspace(ticks[0] , ticks[-2], len(ticks));
+
+    #cax = plt.colorbar(mat, ticks=tickpos)
+    #cax.set_ticklabels(ticks)
+    
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("bottom", size="5%", pad=0.1)
+    cb = fig.colorbar(res, cax=cax, orientation = 'horizontal', ticks=tickpos+0.5)
+    cb.ax.set_xticklabels(np.arange(len(tickpos)))
+
+    #Axes
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    ax.set_xticks(np.arange(width)+0.5)
+    ax.set_xticklabels(x_labels, rotation='vertical')
+    #ax.xaxis.labelpad = 0.1
+    ax.set_yticks(np.arange(height)+0.5)
+    ax.set_yticklabels(y_labels , rotation='horizontal')
+
+    for x in xrange(conf_arr.shape[0]):
+        for y in xrange(conf_arr.shape[1]):
+            textcolor = 'black'
+            if conf_arr[x,y] >= 6:
+                textcolor = 'white'
+            if conf_arr[x,y]==0:
+                continue
+            ax.annotate(int(conf_arr[x,y]), xy=(y+0.5, x+0.5),  horizontalalignment='center', verticalalignment='center', color=textcolor, fontsize=12)
+    plt.tight_layout()
+    plt.savefig(outputname)
+    #plt.show()
+    
+
+def plotPerThresholdAcuracyHindustaniConfig2():
+    """
+    Final thesis plot
+    """
+
+    cc_dir = 'ClusteringCoffs/hindustani/config2/clusteringCoffs_Sep2016/'
+    thsld_dir =  'PhraseBased/Hindustani/thresholds'
+    thslds = range(3,15)
+    plotName = str("plots/HindustaniConfig2_Accuracy_vs_CCdiff.pdf")
+    suffix = '_config2'
+    
+    #lets read all the clustering coffs
+    cc, cc_rand = readClusterinCoffCurve(cc_dir, 30,suffix)
+    cc_diff = {}
+    for ii in range(25):
+        cc_diff[ii] = cc[ii]-cc_rand[ii]
+    
+    #lets read accuracies for all the files with different distance threhsolds
+    range_thslds = thslds
+    cnt=1
+    accuracy = {}
+    for ii in range_thslds:
+        data = pickle.load(open(os.path.join(thsld_dir, 'config_%d'%cnt, 'experiment_results.pkl')))
+        cnt+=1
+        accuracy[ii] = data['var2']['accuracy']
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax2 = ax1.twinx()
+    plt.hold(True)
+    fsize = 18
+    fsize2 = 16
+    
+    ax1.set_xlabel("$\Delta''$ (bin index)", fontsize = fsize)
+    ax1.set_ylabel("Clustering Coefficient $C$", fontsize = fsize, labelpad=10)
+    ax2.set_ylabel("Accuracy (%)", fontsize = fsize, labelpad=10)
+    
+    pLeg = []
+    markers = ['.', 'o', 's', '^', '<', '>', 'p']    
+    colors = ['r', 'b', 'm', 'c', 'g', 'k']
+    colors_dotted = ['r--', 'b--', 'm--', 'c--', 'g--', 'k--']
+
+    p, = ax1.plot(range_thslds, [cc_diff[i] for i in range_thslds], 'r--', linewidth=2)
+    plt.xticks(np.arange(min(range_thslds), max(range_thslds)+1, 1))
+    pLeg.append(p)
+    p, = ax2.plot(range_thslds, [accuracy[i]*100 for i in range_thslds], 'b', linewidth=2)
+    pLeg.append(p)
+    
+    # ax1.set_ylim([.1,0.28])
+    # ax1.set_xlim([min(thslds),max(thslds)])
+    # ax2.set_ylim([30,75])
+    
+    xLim = ax1.get_xlim()
+    yLim = ax1.get_ylim()    
+    #ax1.set_aspect((xLim[1]-xLim[0])/(2*float(yLim[1]-yLim[0])))
+    
+    ax1.set(adjustable='box-forced',
+    aspect=((xLim[1]-xLim[0])/(2*float(yLim[1]-yLim[0]))))
+    
+    xLim = ax2.get_xlim()
+    yLim = ax2.get_ylim()
+    #ax2.set_aspect((xLim[1]-xLim[0])/(2*float(yLim[1]-yLim[0])))
+    
+    ax2.set(adjustable='box-forced',
+    aspect=((xLim[1]-xLim[0])/(2*float(yLim[1]-yLim[0]))))
+    
+    
+    plt.legend(pLeg, ['$C(\mathcal{G})-C(\mathcal{G}_r)$', 'Accuracy of $\mathrm{M}_\mathrm{VSM}$'], fontsize = fsize, loc=1)
+    ax1.tick_params(axis='both', which='major', labelsize=fsize2)
+    ax2.tick_params(axis='both', which='major', labelsize=fsize2)
+    
+    if isinstance(plotName, int):
+        plt.show()
+    elif isinstance(plotName, str):
+        fig.savefig(plotName, bbox_inches='tight')
+
+def plotPerThresholdAcuracyCarnaticConfig3():
+    """
+    Final thesis plot
+    """
+
+    cc_dir = 'ClusteringCoffs/carnatic/config3/clusteringCoffs_Sep2016/'
+    thsld_dir =  'PhraseBased/Carnatic/thresholds'
+    thslds = range(3,15)
+    plotName = str("plots/CarnaticConfig3_Accuracy_vs_CCdiff.pdf")
+    suffix = '_config3'
+    
+    #lets read all the clustering coffs
+    cc, cc_rand = readClusterinCoffCurve(cc_dir, 30,suffix)
+    cc_diff = {}
+    for ii in range(25):
+        cc_diff[ii] = cc[ii]-cc_rand[ii]
+    
+    #lets read accuracies for all the files with different distance threhsolds
+    range_thslds = thslds
+    cnt=1
+    accuracy = {}
+    for ii in range_thslds:
+        data = pickle.load(open(os.path.join(thsld_dir, 'config_%d'%cnt, 'experiment_results.pkl')))
+        cnt+=1
+        accuracy[ii] = data['var2']['accuracy']
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax2 = ax1.twinx()
+    plt.hold(True)
+    fsize = 18
+    fsize2 = 16
+    
+    ax1.set_xlabel("$\Delta''$ (bin index)", fontsize = fsize)
+    ax1.set_ylabel("Clustering Coefficient $C$", fontsize = fsize, labelpad=10)
+    ax2.set_ylabel("Accuracy (%)", fontsize = fsize, labelpad=10)
+    
+    pLeg = []
+    markers = ['.', 'o', 's', '^', '<', '>', 'p']    
+    colors = ['r', 'b', 'm', 'c', 'g', 'k']
+    colors_dotted = ['r--', 'b--', 'm--', 'c--', 'g--', 'k--']
+
+    p, = ax1.plot(range_thslds, [cc_diff[i] for i in range_thslds], 'r--', linewidth=2)
+    plt.xticks(np.arange(min(range_thslds), max(range_thslds)+1, 1))
+    pLeg.append(p)
+    p, = ax2.plot(range_thslds, [accuracy[i]*100 for i in range_thslds], 'b', linewidth=2)
+    pLeg.append(p)
+    
+    # ax1.set_ylim([.1,0.28])
+    # ax1.set_xlim([min(thslds),max(thslds)])
+    # ax2.set_ylim([30,75])
+    
+    xLim = ax1.get_xlim()
+    yLim = ax1.get_ylim()    
+    #ax1.set_aspect((xLim[1]-xLim[0])/(2*float(yLim[1]-yLim[0])))
+    
+    ax1.set(adjustable='box-forced',
+    aspect=((xLim[1]-xLim[0])/(2*float(yLim[1]-yLim[0]))))
+    
+    xLim = ax2.get_xlim()
+    yLim = ax2.get_ylim()
+    #ax2.set_aspect((xLim[1]-xLim[0])/(2*float(yLim[1]-yLim[0])))
+    
+    ax2.set(adjustable='box-forced',
+    aspect=((xLim[1]-xLim[0])/(2*float(yLim[1]-yLim[0]))))
+    
+    
+    plt.legend(pLeg, ['$C(\mathcal{G})-C(\mathcal{G}_r)$', 'Accuracy of $\mathrm{M}_\mathrm{VSM}$'], fontsize = fsize, loc=1)
+    ax1.tick_params(axis='both', which='major', labelsize=fsize2)
+    ax2.tick_params(axis='both', which='major', labelsize=fsize2)
+    
+    if isinstance(plotName, int):
+        plt.show()
+    elif isinstance(plotName, str):
+        fig.savefig(plotName, bbox_inches='tight')        
+
+            
+def get_confusion_matrix_data_sertan(filename):
+    
+    data = json.load(open(filename,'r'))
+    
+    gt_label = []
+    pd_label = []
+    for d in data:
+        gt_label.append(d['annotated_mode'])
+        pd_label.append(d['estimated_mode'])
+    
+    raga_ind_map = {}
+    ind_raga_map = {}
+    
+    u_ragas = np.unique(np.array(gt_label))
+    
+    for ii, u in enumerate(u_ragas):
+        raga_ind_map[u] = ii
+        ind_raga_map[ii] = u
+    
+    cnf_mtx = np.zeros((len(u_ragas), len(u_ragas)))
+    
+    
+    for ii, gt in enumerate(gt_label):
+        cnf_mtx[raga_ind_map[gt], raga_ind_map[pd_label[ii]]]+=1
+    
+    return cnf_mtx, raga_ind_map
+
+def plot_confusion_matrix_sertan(raga_name_map_file, sertan_results_file, outputname):
+    
+    raga_name_map = json.load(codecs.open(raga_name_map_file,'r', encoding = 'utf-8'))
+    conf_arr,raga_names =  get_confusion_matrix_data_sertan(sertan_results_file)
+    
+    y_labels = []
+    x_labels = []
+    for ii, r in enumerate(raga_names):
+        y_labels.append('R'+str(ii+1))
+        x_labels.append(y_labels[-1] + '-'+raga_name_map[r])
+    
+    width = len(conf_arr)
+    height = len(conf_arr[0])
+ 
+    fig = plt.figure(figsize=(14,14))
+    #fig = plt.figure()
+    plt.clf()
+    ax = fig.add_subplot(111)
+    ax.set_aspect(1)
+    ax.grid(which='major')
+    cmap_local = plt.get_cmap('binary', np.max(conf_arr)-np.min(conf_arr)+1)
+    res = ax.matshow(np.array(conf_arr), #cmap=plt.cm.binary, 
+                    interpolation='nearest', aspect='1', cmap=cmap_local,
+                    ##Commenting out this line sets labels correctly,
+                    ##but the grid is off
+                    extent=[0, width, height, 0], vmin =np.min(conf_arr)-.5, vmax = np.max(conf_arr)+0.5,
+                    )
+    ticks = np.arange(np.min(conf_arr),np.max(conf_arr)+1)
+    tickpos = np.linspace(ticks[0] , ticks[-1] , len(ticks));
+    #cax = plt.colorbar(mat, ticks=tickpos)
+    #cax.set_ticklabels(ticks)
+    
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("bottom", size="5%", pad=0.4)
+    cb = fig.colorbar(res, cax=cax, orientation = 'horizontal', ticks=tickpos)
+
+    #Axes
+    ax.set_xticks(range(width))
+    ax.set_xticklabels(x_labels, rotation='vertical')
+    ax.xaxis.labelpad = 0.5
+    ax.set_yticks(range(height))
+    ax.set_yticklabels(y_labels , rotation='horizontal')
+    #plt.tight_layout()
+    plt.savefig(outputname)
+    #plt.show()
+    for x in xrange(conf_arr.shape[0]):
+        for y in xrange(conf_arr.shape[1]):
+            textcolor = 'black'
+            if conf_arr[x,y] >= 6:
+                textcolor = 'white'
+            if conf_arr[x,y]==0:
+                continue
+            ax.annotate(int(conf_arr[x,y]), xy=(y+0.5, x+0.5),  horizontalalignment='center', verticalalignment='center', color=textcolor, fontsize=12)
+    plt.tight_layout()
+    plt.savefig(outputname)
+    #plt.show()
