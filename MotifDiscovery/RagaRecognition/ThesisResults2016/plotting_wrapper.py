10e830b20523b639ae4c21d0c8fb0b56f607cb14
Sankalp Gulati <sankalp.gulati@upf.edu>
2016-09-04 18:45:48 +0200

    Made generic confusion matrix plotter for all methods

--- MotifDiscovery/RagaRecognition/ThesisResults2016/plotting_wrapper.py
+++ MotifDiscovery/RagaRecognition/ThesisResults2016/plotting_wrapper.py
@@ -11,7 +11,7 @@ import matplotlib as mpl
 mpl.rcParams['text.usetex'] = True
 mpl.rcParams['text.latex.unicode'] = True
 mpl.rc('font',family='Times New Roman')
-
+from sklearn.metrics import confusion_matrix
 baseClusterCoffFileName = '_ClusteringCoff'
 randomizationSuffix = '_RANDOM'
 baseNetworkPropFileName = '_NetworkInfo'
@@ -92,71 +92,6 @@ def plotClusteringCoff(root_dir, nFiles, suffix = '', plotName=-1, legData = [])
 #def plot_confusion_matrix(result_file, raga_uuid_name_file):
 
 
-def plot_confusion_matrix_Phrase(raga_name_map_file, result_file, outputname):
-    
-    raga_name_map = json.load(codecs.open(raga_name_map_file,'r', encoding = 'utf-8'))
-    results = pickle.load(open(result_file, 'r'))
-    conf_arr = results['var2']['cm'][0]
-    raga_names = results['var2']['mapping']
-    
-    y_labels = []
-    x_labels = []
-    for ii, r in enumerate(raga_names):
-        y_labels.append(u'R'+str(ii+1))
-        x_labels.append(y_labels[-1] + '-'+raga_name_map[r])
-    
-    width = len(conf_arr)
-    height = len(conf_arr[0])
- 
-    fig = plt.figure(figsize=(14,14))
-    #fig = plt.figure()
-    plt.clf()
-    ax = fig.add_subplot(111)
-    ax.set_aspect(1)
-    ax.grid(which='major')
-    cmap_local = plt.get_cmap('binary', np.max(conf_arr)-np.min(conf_arr)+1)
-    #res = ax.matshow(np.array(conf_arr), #cmap=plt.cm.binary, 
-    #                interpolation='nearest', aspect='1', cmap=cmap_local,
-    #                ##Commenting out this line sets labels correctly,
-    #                ##but the grid is off
-    #                extent=[0, width, height, 0],
-    #                vmin =np.min(conf_arr)-.5, vmax = np.max(conf_arr)+0.5,
-    #                )
-    res = ax.pcolor(np.array(conf_arr), cmap=cmap_local, edgecolor='black', linestyle=':', lw=1)
-    
-    ticks = np.arange(np.min(conf_arr),np.max(conf_arr)+2)
-    tickpos = np.linspace(ticks[0] , ticks[-2], len(ticks));
-
-    #cax = plt.colorbar(mat, ticks=tickpos)
-    #cax.set_ticklabels(ticks)
-    
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("bottom", size="5%", pad=0.1)
-    cb = fig.colorbar(res, cax=cax, orientation = 'horizontal', ticks=tickpos+0.5)
-    cb.ax.set_xticklabels(np.arange(len(tickpos)))
-
-    #Axes
-    ax.invert_yaxis()
-    ax.xaxis.tick_top()
-
-    ax.set_xticks(np.arange(width)+0.5)
-    ax.set_xticklabels(x_labels, rotation='vertical')
-    #ax.xaxis.labelpad = 0.1
-    ax.set_yticks(np.arange(height)+0.5)
-    ax.set_yticklabels(y_labels , rotation='horizontal')
-
-    for x in xrange(conf_arr.shape[0]):
-        for y in xrange(conf_arr.shape[1]):
-            textcolor = 'black'
-            if conf_arr[x,y] >= 6:
-                textcolor = 'white'
-            if conf_arr[x,y]==0:
-                continue
-            ax.annotate(int(conf_arr[x,y]), xy=(y+0.5, x+0.5),  horizontalalignment='center', verticalalignment='center', color=textcolor, fontsize=12)
-    plt.tight_layout()
-    plt.savefig(outputname)
-    #plt.show()
-    
 
 def plotPerThresholdAcuracyHindustaniConfig2():
     """
@@ -310,36 +245,80 @@ def plotPerThresholdAcuracyCarnaticConfig3():
     elif isinstance(plotName, str):
         fig.savefig(plotName, bbox_inches='tight')        
 
-            
-def get_confusion_matrix_data_sertan(filename):
-    
+
+def get_GT_PD_labels_VSM(filename, version):
+    """
+    To fetch ground truth and predicted labels in output files produced in VSM method
+    """
+    data = pickle.load(open(filename,'r'))
+    mapping = data[version]['mapping']
+
+    gt_labels = []
+    pred_labels = []
+
+    for lab in data[version]['gt_label']:
+        gt_labels.append(mapping[lab])
+
+    for lab in data[version]['pred_label']:
+        pred_labels.append(mapping[lab[0]])
+
+    return gt_labels, pred_labels
+
+
+def get_GT_PD_labels_TDMS(filename, version):
+    """
+    To fetch ground truth and predicted labels in output files produced in TDMS method
+    """
+    data = pickle.load(open(filename,'r'))
+    return data[version]['gt_label'], data['var1']['pred_label']
+
+
+def get_GT_PD_labels_PCD(filename, version):
+    """
+    To fetch ground truth and predicted labels in output files produced in PCD method (Sertan)
+    """
     data = json.load(open(filename,'r'))
-    
-    gt_label = data['true_raag']
-    pd_label = data['estimated_raag']
+    return data['true_raag'], data['estimated_raag']
 
-    raga_ind_map = {}
-    ind_raga_map = {}
-    
-    u_ragas = np.unique(np.array(gt_label))
-    
-    for ii, u in enumerate(u_ragas):
-        raga_ind_map[u] = ii
-        ind_raga_map[ii] = u
-    
-    cnf_mtx = np.zeros((len(u_ragas), len(u_ragas)))
-    
-    
-    for ii, gt in enumerate(gt_label):
-        cnf_mtx[raga_ind_map[gt], raga_ind_map[pd_label[ii]]]+=1
-    
-    return cnf_mtx, raga_ind_map
 
-def plot_confusion_matrix_sertan(raga_name_map_file, sertan_results_file, outputname):
+
+
+def plotAllConfusionMatricesThesis():
+    """
+    This function plots all the confusion matrices to be used in the thesis
+    """
+    raga_name_map_file  = 'raga_name_mapping.json'
+
+    vsm_cmd = ['vsm', 'PhraseBased/Carnatic/paperTable_ONLYVAR2/config_20/experiment_results.pkl', 'var2', 'plots/CM_vsm_cmd_var2.pdf']
+    vsm_hmd = ['vsm', 'PhraseBased/Hindustani/paperTable_ONLYVAR2/config_20/experiment_results.pkl', 'var2', 'plots/CM_vsm_hmd_var2.pdf']
+
+    tdms_cmd = ['tdms', '../ISMIR_2016/ISMIR_2016_Table_Results/carnatic/M_KL/experiment_results.pkl', 'var1', 'plots/CM_tdms_cmd_var1.pdf']
+    tdms_hmd = ['tdms', '../ISMIR_2016/ISMIR_2016_Table_Results/hindustani/M_KL/experiment_results.pkl', 'var1', 'plots/CM_tdms_hmd_var1.pdf']
+
+    pcd_cmd = ['pcd', '../ISMIR_2016/ISMIR_2016_Table_Results/carnatic/E_PCD/carnatic_40_classes_results.json', 'xxx', 'plots/CM_pcd_cmd.pdf']
+    pcd_hmd = ['pcd', '../ISMIR_2016/ISMIR_2016_Table_Results/hindustani/E_PCD/hindustani_30_classes_results.json', 'xxx', 'plots/CM_pcd_hmd.pdf']
+
+
+
+    methods = [vsm_cmd, vsm_hmd, tdms_cmd, tdms_hmd, pcd_cmd, pcd_hmd]
+
+    for m in methods:
+        plot_confusion_matrix(raga_name_map_file, m[1], m[3], m[2], m[0])
+
+
+            
+def plot_confusion_matrix(raga_name_map_file, result_file, outputname, version, id_method):
     
+    fncs = {'vsm': get_GT_PD_labels_VSM, 'tdms':get_GT_PD_labels_TDMS, 'pcd': get_GT_PD_labels_PCD}
     raga_name_map = json.load(codecs.open(raga_name_map_file,'r', encoding = 'utf-8'))
-    conf_arr,raga_names =  get_confusion_matrix_data_sertan(sertan_results_file)
     
+    labels, predictions = fncs[id_method](result_file, version)
+    
+
+    u_labels = np.unique(labels)
+    conf_arr = confusion_matrix(labels, predictions, labels = u_labels)
+    raga_names = u_labels
+   
     y_labels = []
     x_labels = []
     for ii, r in enumerate(raga_names):
@@ -356,30 +335,36 @@ def plot_confusion_matrix_sertan(raga_name_map_file, sertan_results_file, output
     ax.set_aspect(1)
     ax.grid(which='major')
     cmap_local = plt.get_cmap('binary', np.max(conf_arr)-np.min(conf_arr)+1)
-    res = ax.matshow(np.array(conf_arr), #cmap=plt.cm.binary, 
-                    interpolation='nearest', aspect='1', cmap=cmap_local,
-                    ##Commenting out this line sets labels correctly,
-                    ##but the grid is off
-                    extent=[0, width, height, 0], vmin =np.min(conf_arr)-.5, vmax = np.max(conf_arr)+0.5,
-                    )
-    ticks = np.arange(np.min(conf_arr),np.max(conf_arr)+1)
-    tickpos = np.linspace(ticks[0] , ticks[-1] , len(ticks));
+    #res = ax.matshow(np.array(conf_arr), #cmap=plt.cm.binary, 
+    #                interpolation='nearest', aspect='1', cmap=cmap_local,
+    #                ##Commenting out this line sets labels correctly,
+    #                ##but the grid is off
+    #                extent=[0, width, height, 0],
+    #                vmin =np.min(conf_arr)-.5, vmax = np.max(conf_arr)+0.5,
+    #                )
+    res = ax.pcolor(np.array(conf_arr), cmap=cmap_local, edgecolor='black', linestyle=':', lw=1)
+    
+    ticks = np.arange(np.min(conf_arr),np.max(conf_arr)+2)
+    tickpos = np.linspace(ticks[0] , ticks[-2], len(ticks));
+
     #cax = plt.colorbar(mat, ticks=tickpos)
     #cax.set_ticklabels(ticks)
     
     divider = make_axes_locatable(ax)
-    cax = divider.append_axes("bottom", size="5%", pad=0.4)
-    cb = fig.colorbar(res, cax=cax, orientation = 'horizontal', ticks=tickpos)
+    cax = divider.append_axes("bottom", size="5%", pad=0.1)
+    cb = fig.colorbar(res, cax=cax, orientation = 'horizontal', ticks=tickpos+0.5)
+    cb.ax.set_xticklabels(np.arange(len(tickpos)),fontsize = 14)
 
     #Axes
-    ax.set_xticks(range(width))
-    ax.set_xticklabels(x_labels, rotation='vertical')
-    ax.xaxis.labelpad = 0.5
-    ax.set_yticks(range(height))
-    ax.set_yticklabels(y_labels , rotation='horizontal')
-    #plt.tight_layout()
-    plt.savefig(outputname)
-    #plt.show()
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    ax.set_xticks(np.arange(width)+0.5)
+    ax.set_xticklabels(x_labels, rotation='vertical', fontsize = 14)
+    #ax.xaxis.labelpad = 0.1
+    ax.set_yticks(np.arange(height)+0.5)
+    ax.set_yticklabels(y_labels , rotation='horizontal', fontsize = 14)
+
     for x in xrange(conf_arr.shape[0]):
         for y in xrange(conf_arr.shape[1]):
             textcolor = 'black'
@@ -390,4 +375,3 @@ def plot_confusion_matrix_sertan(raga_name_map_file, sertan_results_file, output
             ax.annotate(int(conf_arr[x,y]), xy=(y+0.5, x+0.5),  horizontalalignment='center', verticalalignment='center', color=textcolor, fontsize=12)
     plt.tight_layout()
     plt.savefig(outputname)
-    #plt.show()
